c1d030eff65ed375f1f29d101017628d0af47f7e
kaikai-sk <1431173103@qq.com>
2017-08-03 10:38:00 +0800

    添加了一行

--- test/IDEATest/Test.java
+++ test/IDEATest/Test.java
@@ -4,6 +4,11 @@ public class Test
 {
 	public static void main(String[] args)
 	{
-	    System.out.println("Hello IDEA");
+		System.out.println("Hello IDEA");
+		//F2    Ctrl+1 键快捷修复
+		//
+		long a=100L;
+		
+		
 	}
 }
